351a18210696399cb47f38f68b4d724139e99361
anaxnds <37352154+anaxnds@users.noreply.github.com>
2019-07-24 12:00:09 -0300

    Update 1020.py

--- 1020.py
+++ 1020.py
@@ -1,8 +1,10 @@
        	#!usr//bin/env python
        	# --coding: utf-8 --
-        int(input())
-	ano = int(valor / 365)
-	mes = int((valor%365/30))
-	dia = int(valor%365%30)
-	
-	print('%d ano(s)\n%d mes(es)\n%d dia(s)' %(ano,mes,dia))
+        n = int(input())
+	ano = int(n) 365
+	mes = int(n) - (int(ano) * 365)) / 30
+	dia = int(n) - (int(ano) * 365) - (int(mes) * 30) 
+        
+        print (int(ano), “ano(s)”)
+        print (int(mes), “mes(es)”)
+        print (int(dias), “dia(s)”)
